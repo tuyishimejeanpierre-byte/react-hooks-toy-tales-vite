97c1370901982dbda60f288f4330b27dc27aaa71
tuyishimejeanpierre-byte <tuyishime.jeanpierre@student.moringaschool.com>
2026-05-11 08:46:02 +0300

    Display toys on page load

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -7,18 +7,32 @@ import ToyContainer from "./ToyContainer";
 function App() {
   const [showForm, setShowForm] = useState(false);
 
+  // ✅ ADD STATE FOR TOYS
+  const [toys, setToys] = useState([]);
+
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  // ✅ FETCH TOYS ON PAGE LOAD
+  useEffect(() => {
+    fetch("http://localhost:6001/toys")
+      .then((res) => res.json())
+      .then((data) => setToys(data));
+  }, []);
+
   return (
     <>
       <Header />
+
       {showForm ? <ToyForm /> : null}
+
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+
+      {/* ✅ PASS TOYS DOWN */}
+      <ToyContainer toys={toys} />
     </>
   );
 }
